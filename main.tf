14e629ccbe02c893f45d66d91fc1cb0efb7389c1
bnhlong <longbnh.vn@gmail.com>
2025-05-22 13:08:20 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,7 @@ resource "aws_instance" "example" {
   ami           = "ami-01938df366ac2d954"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0b03d46e315983c7f"
+  tags = {
+    Name = "my-ec2-instance"
+  }
 }
